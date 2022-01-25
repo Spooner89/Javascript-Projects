20ed5587dd8eb75dc248ac53ffec17f650aa541a
Spooner89 <92983380+Spooner89@users.noreply.github.com>
2022-01-25 18:58:32 +0000

    Assignment Submission # 2

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    var sentence = "Do you always do ";
+    sentence += "what someone tells you to do :)";
+    document.getElementById("dont").innerHTML = sentence; //This function is allowing the sentence to be inserted into the webpage apon someone clicking the CLICK ME! section on the index.html 
+}
