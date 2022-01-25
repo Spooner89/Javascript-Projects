72038c6f5a9a2c52d28e477505b9a205b12723c8
Spooner89 <92983380+Spooner89@users.noreply.github.com>
2022-01-25 17:33:00 +0000

    Assignment Submission # 1

---
+++ Basic Javascript Projects/JS/main.js
@@ -0,0 +1,32 @@
+window.alert (" Westley Spooner ") //Opening up an alert box when you open the web page 
+document.write (" Hello World! ") //adding the text "Hello World to the webpage"
+var A = (" Welcome to the world of Javascript. ")
+document.write (A) // Creating a variable 
+
+document.write (" Captain America said, \" Dr Banner. Now might be a good time for you to get angry\" Thats my secret, captain. im always angry\" Said Banner ") // A string that contains an apostrophe and/or quotations marks. 
+
+var B = ("\"For me, becoming isn\'t about arriving somewhere or achieving a certain aim." 
++"I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self."
++"The journey doesn\'t end."
++"- Michelle Obama ")
+document.write (B) //concatenated string value added as a variable 
+
+
+
+var family = "The Spooners", Dad = " Andrew ", Mum = " Wendy ", Sister = " Olivia ", Brother = " Sam "; // Setting mutiple variables 
+var family = family.fontcolor("blue");
+var Dad = Dad.fontcolor("green");
+var Mum = Mum.fontcolor("pink");
+var Sister = Sister.fontcolor ("yellow");
+var Brother = Brother.fontcolor("orange"); // Setting a font color for each variable 
+document.write (Dad)
+document.write (Mum)
+document.write (Sister)
+document.write (Brother)
+document.write (family) // instructing the pc to input each variable to the web page 
+document.write (109474934938+472937291038); // Statement that contains an expression 
+
+let text1 = "Hello";
+let text2 = "world!";
+let text3 = "Have a nice day!";
+let result = text1.concat(" ", text2, " ", text3);
